68b84bc1fef7668fdff91d7d9373dc1d4c4a78ed
AchintyaAkula <achintya.akula@gmail.com>
2026-01-31 12:36:08 -0800

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Enums/Alliance.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Enums/Alliance.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Enums
 
 import com.pedropathing.geometry.Pose
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
 import dev.nextftc.units.Measure
 import dev.nextftc.units.unittypes.DistanceUnit
 import dev.nextftc.units.unittypes.Inches
@@ -8,7 +9,13 @@ import dev.nextftc.units.unittypes.Inches
 enum class Alliance {
     RED {
         override val goalPose: Pose = Pose(this.fieldWidth.magnitude, this.fieldLength.magnitude)
-        override val turretTargetPose: Pose = Pose(this.goalPose.x - this.turretAimOffsetX.magnitude, this.goalPose.y - this.turretAimOffsetY.magnitude)
+        override val farTurretTargetPose: Pose = Pose(this.goalPose.x - this.turretAimCloseOffsetX.magnitude, this.goalPose.y)
+        override val closeTurretTargetPose: Pose = Pose(this.goalPose.x - this.turretFarAimOffsetX.magnitude, this.goalPose.y)
+
+        override fun turretTargetPose(): Pose {
+            return if (follower.pose.distanceFrom(goalPose) > 100) { this.farTurretTargetPose}
+                   else { this.closeTurretTargetPose }
+        }
 
         override val firstResetPose: Pose = Pose(0.0,0.0)
         override val secondResetPose: Pose = Pose(0.0,0.0)
@@ -18,7 +25,13 @@ enum class Alliance {
     },
     BLUE {
         override val goalPose: Pose = Pose(0.0, this.fieldLength.magnitude)
-        override val turretTargetPose: Pose = Pose(this.goalPose.x + this.turretAimOffsetX.magnitude, this.goalPose.y - this.turretAimOffsetY.magnitude)
+        override val farTurretTargetPose: Pose = Pose(this.goalPose.x + this.turretAimCloseOffsetX.magnitude, this.goalPose.y)
+        override val closeTurretTargetPose: Pose = Pose(this.goalPose.x + this.turretFarAimOffsetX.magnitude, this.goalPose.y)
+
+        override fun turretTargetPose(): Pose {
+            return if (follower.pose.distanceFrom(goalPose) > 100) { this.farTurretTargetPose}
+                   else { this.closeTurretTargetPose }
+        }
 
         override val firstResetPose: Pose = Pose(0.0,0.0)
         override val secondResetPose: Pose = Pose(0.0,0.0)
@@ -28,7 +41,9 @@ enum class Alliance {
     };
 
     abstract val goalPose: Pose
-    abstract val turretTargetPose: Pose
+    abstract val farTurretTargetPose: Pose
+    abstract val closeTurretTargetPose: Pose
+    abstract fun turretTargetPose(): Pose
 
     abstract val firstResetPose: Pose
     abstract val secondResetPose: Pose
@@ -38,6 +53,6 @@ enum class Alliance {
 
     open val fieldWidth: Measure<DistanceUnit> = Inches.of(141.5)
     open val fieldLength: Measure<DistanceUnit> = Inches.of(141.5)
-    open val turretAimOffsetX: Measure<DistanceUnit> = Inches.of(1.0)
-    open val turretAimOffsetY: Measure<DistanceUnit> = Inches.of(0.0)
+    open val turretAimCloseOffsetX: Measure<DistanceUnit> = Inches.of(6.0)
+    open val turretFarAimOffsetX: Measure<DistanceUnit> = Inches.of(14.5)
 }
