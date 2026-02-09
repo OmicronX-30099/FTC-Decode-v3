00cddc75dd35c8e38371b44e8592ca05ba105419
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-02-09 09:51:20 -0800

    Finished Blue TeleOp
    Tilt needs to be added(will create subsystems later)
    Need to put imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBlue.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBlue.kt
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.TeleOp
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.ftc.NextFTCOpMode
+import org.firstinspires.ftc.teamcode.Util.addSubsystems
+import org.firstinspires.ftc.teamcode.Util.includePedro
+
+@TeleOp(name = "Blue TeleOp", group = "TeleOp")
 class TeleOpBlue: NextFTCOpMode() {
     init {
         addSubsystems(Shooter, Load)
@@ -27,6 +33,35 @@ class TeleOpBlue: NextFTCOpMode() {
         Gamepads.gamepad1.leftTrigger.greaterThan(0.0).and(Gamepads.gamepad1.rightTrigger.inRange(0.0..0.0))
             .whenBecomesTrue { Rollers.run(-1.0,-1.0) }
             .whenBecomesFalse { Rollers.run(0.0,0.0) }
-            
+        Gamepads.gamepad1.rightBumper
+            .whenBecomesTrue ( Load.shootTripleCommand )
+        Gamepads.gamepad1.cross.or(Gamepads.gamepad1.rightBumper)
+            .whenBecomesTrue {
+                if (Shooter.flywheelState == FlywheelState.AUTO_AIM) { Shooter.flywheelState = FlywheelState.IDLE }
+                else { Shooter.flywheelState = FlywheelState.AUTO_AIM }
+            }
+        Gamepads.gamepad1.dPadUp.or(Gamepads.gamepad2.dPadUp)
+            .whenBecomesTrue {  }
+        Gamepads.gamepad1.dpadDown.or(Gamepads.gamepad2.dPadDown)
+            .whenBecomesTrue {  }
+        Gamepads.gamepad2.dPadLeft
+            .whenBecomesTrue { Shooter.moveTurretBy(5.0) }
+        Gamepads.gamepad2.dPadRight
+            .whenBecomesTrue { Shooter.moveTurretBy(-5.0) }
+        Gamepads.gamepad2.square
+            .whenBecomesTrue { follower.pose = ROBOT.currAlliance.resetPoses.resetPose2 }
+        Gamepads.gamepad2.triangle
+            .whenBecomesTrue { follower.pose = ROBOT.currAlliance.resetPoses.resetPose3 }
+        Gamepads.gamepad2.circle
+            .whenBecomesTrue { follower.pose = ROBOT.currAlliance.resetPoses.resetPose4 }
+        Gamepads.gamepad2.cross
+            .whenBecomesTrue { follower.pose = ROBOT.currAlliance.resetPoses.resetPose5 }
+        Gamepads.gamepad1.square
+            .whenBecomesTrue { follower.pose = ROBOT.currAlliance.resetPoses.resetPose1 }
+    }
+
+    override fun onUpdate() {
+        Shooter.update()
+        telemetry.update()
     }
 }
