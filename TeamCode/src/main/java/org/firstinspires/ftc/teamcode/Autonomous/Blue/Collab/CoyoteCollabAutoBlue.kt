d29ec43dd8df774fd1aa1b8d0e7975a9567d2c1c
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-02-10 10:19:39 -0800

    Add CoyoteCollabAutoBlue autonomous routine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blue/Collab/CoyoteCollabAutoBlue.kt
@@ -0,0 +1,204 @@
+package org.firstinspires.ftc.teamcode.Autonomous
+
+import com.pedropathing.geometry.BezierCurve
+import com.pedropathing.geometry.BezierLine
+import com.pedropathing.geometry.Pose
+import com.pedropathing.paths.PathChain
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import dev.nextftc.core.commands.delays.Delay
+import dev.nextftc.core.commands.groups.SequentialGroup
+import dev.nextftc.core.commands.utility.InstantCommand
+import dev.nextftc.extensions.pedro.FollowPath
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
+import dev.nextftc.ftc.NextFTCOpMode
+import org.firstinspires.ftc.teamcode.Constants.PedroConstants
+import org.firstinspires.ftc.teamcode.Enums.Alliance
+import org.firstinspires.ftc.teamcode.Systems.IntakeSubsystems.IntakeSubsystem
+import org.firstinspires.ftc.teamcode.Systems.IntakeSubsystems.TransferSubsystem
+import org.firstinspires.ftc.teamcode.Systems.PassiveSystem
+import org.firstinspires.ftc.teamcode.Systems.ShooterSystem
+import org.firstinspires.ftc.teamcode.Util.addSubsystems
+import org.firstinspires.ftc.teamcode.Util.currAlliance
+import org.firstinspires.ftc.teamcode.Util.includePedro
+
+@Autonomous(name = "Red Coyote Collab Auto", group = "Coyote Collab Auto", preselectTeleOp = "Red TeleOp")
+class CoyoteCollabAutoRed: NextFTCOpMode() {
+    init {
+        addSubsystems(Shooter, Load, Miscellaneous)
+        includePedro(PedroConstants::createFollower)
+    }
+    private var paths: Array<PathChain> = arrayOf()
+
+    override fun onInit() {
+        ROBOT.currAlliance = Alliance.RED
+    }
+
+    override fun onStartButtonPressed() {
+        buildPaths()
+        follower.setStartingPose(Pose(87.500, 8.000, Math.toRadians(0.0)))
+        val main = SequentialGroup(
+            FollowPath(paths[0]), //go to first shoot
+            Delay(0.5),
+            Load.shootTripleCommand,
+            InstantCommand {
+                Rollers.run(0.25,1.0)
+            },
+            FollowPath(paths[1]), //intake first spike
+            InstantCommand {
+                Rollers.run(0.0,0.0)
+            },
+            //FollowPath(paths[2]),//turn
+            FollowPath(paths[3]),//go to shoot
+            Load.shootTripleCommand,
+            InstantCommand {
+                Rollers.run(0.25,1.0)
+            },
+            Delay(1.0),
+            FollowPath(paths[4]),//intake second p1
+            FollowPath(paths[5]),//intake second p2
+            FollowPath(paths[6]),//intake second p3
+            InstantCommand {
+                Rollers.run(0.0,0.0)
+            },
+            FollowPath(paths[7]),//shoot second
+            Delay(0.25),
+            Load.shootTripleCommand,
+            InstantCommand {
+                Rollers.run(0.25,1.0)
+            },
+            FollowPath(paths[8]), //intake third p1
+            FollowPath(paths[9]), //intake third p2
+            InstantCommand {
+                Rollers.run(0.25,1.0)
+            },
+            FollowPath(paths[10]), //shoot 3rd
+            Delay(0.25),
+            Load.shootTripleCommand,
+            FollowPath(paths[11]), //leave
+        )
+        main.schedule()
+    }
+
+    override fun onUpdate() {
+        Shooter.update()
+        telemetry.update()
+    }
+
+    fun buildPaths() {
+        val shootpreload = follower //path0
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(87.500, 8.000).mirror(), Pose(87.500, 20.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(150.0))
+            .build()
+
+        val firstintake = follower //path1
+            .pathBuilder()
+            .addPath(
+                BezierCurve(
+                    Pose(87.500, 20.000).mirror(),
+                    Pose(100.000, 35.000).mirror(),
+                    Pose(130.000, 35.000).mirror()
+                )
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(150.0), Math.toRadians(180.0))
+            .build()
+
+        val turn = follower //path2
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(130.000,35.000).mirror(), Pose(130.000,35.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(160.0))
+            .build()
+
+        val shootfirst = follower //path3
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(130.000, 35.000).mirror(), Pose(87.500, 20.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(160.0))
+            .build()
+
+        val intakesecondp1 = follower //path4
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(87.500, 20.000).mirror(), Pose(133.000, 27.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(160.0), Math.toRadians(170.0))
+            .build()
+
+        val intakesecondp2 = follower //path5
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(133.000, 27.000).mirror(), Pose(123.000, 25.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(170.0), Math.toRadians(180.0))
+            .build()
+
+        val intakesecondp3 = follower //path6
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(123.000, 25.000).mirror(), Pose(133.000, 25.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(180.0))
+            .build()
+
+        val shootsecond = follower //path7
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(133.000, 25.000).mirror(), Pose(85.000, 25.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(180.0))
+            .build()
+
+        val intake3rdp1 = follower //path8
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(85.000, 25.000).mirror(), Pose(130.000, 11.000).mirror())
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(-165.0))
+            .build()
+
+        val intake3rdp2 = follower //path9
+            .pathBuilder()
+            .addPath(
+                BezierCurve(
+                    Pose(130.000, 11.000).mirror(),
+                    Pose(128.000, 10.000).mirror(),
+                    Pose(133.000, 9.000).mirror()
+                )
+            )
+            .setLinearHeadingInterpolation(Math.toRadians(-15.0), Math.toRadians(0.0))
+            .build()
+
+        val shootthird = follower //path10
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(133.000, 9.000).mirror(), Pose(90.000, 9.000).mirror())
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .build()
+
+        val leave = follower //path11
+            .pathBuilder()
+            .addPath(
+                BezierLine(Pose(90.000, 9.000).mirror(), Pose(110.000, 9.000).mirror())
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .build()
+        paths += shootpreload
+        paths += firstintake
+        paths += turn
+        paths += shootfirst
+        paths += intakesecondp1
+        paths += intakesecondp2
+        paths += intakesecondp3
+        paths += shootsecond
+        paths += intake3rdp1
+        paths += intake3rdp2
+        paths += shootthird
+        paths += leave
+    }
+}
