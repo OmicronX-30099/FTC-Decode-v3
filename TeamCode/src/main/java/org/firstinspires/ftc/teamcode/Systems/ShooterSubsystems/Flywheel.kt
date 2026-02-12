88563822497ab4ad061e36cbd474d10a2da4f3c6
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-02-12 13:57:43 -0800

    Update Flywheel.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ShooterSubsystems/Flywheel.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ShooterSubsystems/Flywheel.kt
@@ -31,6 +31,8 @@ object Flywheel: Subsystem {
     
     internal var flywheelTarget: Double = 0.0
 
+    internal fun isAtTarget(): Boolean { return ((flywheelTarget - 20.0) < flywheelMotors.velocity) && ((flywheelTarget + 40.0) > flywheelMotors.velocity) }
+    
     internal fun update() {
         val pid = flywheelPIDController.calculate(error = flywheelTarget - flywheelMotors.velocity)
         val ff  = flywheelFFController.calculate(flywheelTarget)
