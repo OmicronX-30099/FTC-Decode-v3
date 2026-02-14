a7d377a49928b2bcc89f22629cada9b29203ca2d
AchintyaAkula <achintya.akula@gmail.com>
2026-02-13 17:15:28 -0800

    Revert "asdf"
    This reverts commit 859f124b372c5586290f9b164fb11317ee448535.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nonsense/servo_tester.kt
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Nonsense
+
+import com.bylazar.configurables.annotations.Configurable
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.ftc.NextFTCOpMode
+import dev.nextftc.hardware.impl.ServoEx
+
+@Configurable
+@TeleOp
+class servo_tester: NextFTCOpMode() {
+    init {
+
+    }
+    companion object {
+        @JvmField
+        var pos1 = 1-0.96875
+        @JvmField
+        var pos2 = 0.5
+    }
+    private val left_tilt: ServoEx = ServoEx("lt", -0.1)
+    private val right_t: ServoEx = ServoEx("ft",-0.1)
+
+
+    override fun onUpdate() {
+        left_tilt.position = pos1
+        right_t.position = pos1
+    }
+
+}
