14b7c8d49032812f7455938d31e135ab5261d3d2
AchintyaAkula <achintya.akula@gmail.com>
2026-02-18 07:25:01 -0800

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Load.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Load.kt
@@ -1,19 +1,38 @@
-package org.firstinspires.ftc.teamcode.Systems.LoadSubsystems
+package org.firstinspires.ftc.teamcode.Systems
 
+import dev.nextftc.core.commands.Command
+import dev.nextftc.core.commands.delays.Delay
+import dev.nextftc.core.commands.groups.SequentialGroup
+import dev.nextftc.core.commands.utility.InstantCommand
 import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.core.subsystems.SubsystemGroup
 import dev.nextftc.hardware.impl.MotorEx
 import dev.nextftc.hardware.impl.ServoEx
+
+object Load: SubsystemGroup(Rollers, BIM) {
+    fun shootCommand(iPow: Double, tPow: Double, delay: Double): Command
+        = SequentialGroup(
+            InstantCommand { Rollers .apply { run(iPow, tPow); unlockShooter() } },
+            Delay(delay),
+            InstantCommand { Rollers.apply { stop(); lockShooter() } }
+        )
+}
 
 object Rollers: Subsystem {
     private val intakeMotor: MotorEx = MotorEx("")
     private val transferMotor: MotorEx = MotorEx("")
+    private val shooterGateServo: ServoEx = ServoEx("")
 
     fun intake(pow: Double) { intakeMotor.power = pow }
     fun transfer(pow: Double) { transferMotor.power = -pow }
-    fun run(iPow: Double, tPow: Double) =
-            intake(iPow)
-    .also { transfer(tPow) }
+
+    fun run(iPow: Double, tPow: Double) { intake(iPow); transfer(tPow) }
     fun stop() = run(0.0,0.0)
+
+    fun unlockShooter() { shooterGateServo.position = 0.2 }
+    fun lockShooter() { shooterGateServo.position = 0.5 }
+
+    override fun initialize() { lockShooter() }
 }
 
 object BIM: Subsystem {
