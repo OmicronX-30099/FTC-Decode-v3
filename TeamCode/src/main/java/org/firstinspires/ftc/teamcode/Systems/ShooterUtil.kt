ec4b0c0f672694785f9e322f1d2e0f5e459887d5
AchintyaAkula <achintya.akula@gmail.com>
2026-02-27 21:54:55 -0800

    Turret Subsystem Finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ShooterUtil.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ShooterUtil.kt
@@ -5,9 +5,23 @@ import dev.nextftc.hardware.impl.ServoEx
 import dev.nextftc.hardware.positionable.ServoGroup
 
 object Turret: Subsystem {
-    private val turretServo1 = ServoEx("")
-    private val turretServo2 = ServoEx("")
-    private val turretServos = ServoGroup(turretServo1, turretServo2)
+    private val turretServo1: ServoEx = ServoEx("")
+    private val turretServo2: ServoEx = ServoEx("")
+    private val turretServos: ServoGroup = ServoGroup(turretServo1, turretServo2)
 
-    var turret
+    private const val GEAR_RATIO: Double = 0.9375
+    private const val SERVO_RANGE: Double = 358.75
+
+    private var turretOffset: Double = 0.0
+    var targetTurretAngle: Double = 0.0
+
+    fun offset(by: Double) { turretOffset += by }
+    fun update() { turretServos.position = (normalizeAngle(targetTurretAngle + turretOffset) * (GEAR_RATIO / SERVO_RANGE) + 0.5) }
+}
+
+internal fun normalizeAngle(angDeg: Double): Double {
+    var a = angDeg
+    while (a < 0.0) { a += 360.0 }
+    while (a > 180.0) { a -= 360.0 }
+    return a
 }
