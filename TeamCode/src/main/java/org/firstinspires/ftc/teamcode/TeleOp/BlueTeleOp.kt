a06886e24825a6b88a5eee3e8c2d0997c0f08937
AchintyaAkula <achintya.akula@gmail.com>
2026-02-28 10:58:24 -0800

    Caching Changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BlueTeleOp.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.TeleOp
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.ftc.NextFTCOpMode
+
+@TeleOp(name = "Blue TeleOp", group = "Standard TeleOp")
+class BlueTeleOp: NextFTCOpMode() {
+    init {
+
+    }
+}
