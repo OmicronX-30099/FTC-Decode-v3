f631741f72e0986ff00f3f993533701e9596c0d6
AchintyaAkula <achintya.akula@gmail.com>
2026-02-28 10:43:58 -0800

    Caching Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.kt
@@ -4,13 +4,44 @@ import com.pedropathing.geometry.Pose
 import com.pedropathing.math.Vector
 import dev.nextftc.core.subsystems.SubsystemGroup
 import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
+import dev.nextftc.hardware.impl.ServoEx
+import org.firstinspires.ftc.teamcode.Util.ROBOT
 import kotlin.math.atan2
 import kotlin.math.max
 import kotlin.math.min
 
 object Shooter: SubsystemGroup(Turret, Flywheel) {
+    private val frontRGBLight: ServoEx = ServoEx("")
+    private val backRGBLight: ServoEx = ServoEx("")
     private const val ITERATIONS: Int = 10
     var flywheelState: FlywheelState = FlywheelState.AUTO_AIM
+        private set
+
+    fun update() {
+        when (flywheelState) {
+            FlywheelState.AUTO_AIM -> { updateFlywheel() }
+            FlywheelState.MANUAL -> { Flywheel.update() }
+        }
+        updateTurret()
+    }
+
+    fun flywheelManual() {
+        flywheelState = FlywheelState.MANUAL
+        Flywheel.targetVelocity = Flywheel.IDLE_VELOCITY
+    }
+
+    private fun updateTurret() {
+        val futureVec = getCorrectedVecIterative(ROBOT.shooterPose(), ROBOT.currAlliance.goalPoses.turretGoalPose, follower.velocity)
+        Turret.targetAngle = calculateTurretAngle(futureVec)
+        Turret.update()
+    }
+    private fun updateFlywheel() {
+        val futureVec = getCorrectedVecIterative(ROBOT.shooterPose(), ROBOT.currAlliance.goalPoses.flywheelGoalPose, follower.velocity)
+        Flywheel.targetVelocity = calculateFlywheelVelocity(futureVec.magnitude)
+        Flywheel.update()
+    }
+    private fun calculateFlywheelVelocity(d: Double) = ((0.019454 * d * d) + (2.007 * d) + 1102.62509)
+    private fun calculateTurretAngle(vec: Vector) = Math.toDegrees(atan2(vec.yComponent, vec.xComponent) - follower.pose.heading)
 
     private fun getFlyTime(d: Double): Double {
         val t = (-4.64e-5 * d * d) + (1.51e-2 * d) - 3.48e-1
@@ -20,23 +51,9 @@ object Shooter: SubsystemGroup(Turret, Flywheel) {
         val r = Vector(Pose(targetPose.x-botPose.x, targetPose.y-botPose.y))
         var c = r
         repeat(ITERATIONS) {
-            val t = getFlyTime(c)
+            val t = getFlyTime(c.magnitude)
             c = r.plus(velocity.times(t))
         }
         return c
     }
-
-    private fun updateTurret() {
-        val targetPose: Pose = Pose(134.0,134.0)
-        val futureVec = getCorrectedVecIterative(follower.pose, targetPose, follower.velocity)
-        Turret.targetAngle = calculateTurretAngle(futureVec)
-    }
-    private fun updateFlywheel() {
-        val targetPose: Pose = Pose(134.0,134.0)
-        val futureVec = getCorrectedVecIterative(follower.pose, targetPose, follower.velocity)
-        val d = futureVec.magnitude
-        Flywheel.targetVelocity = calculateFlywheelVelocity(futureVec.magnitude)
-    }
-    private fun calculateFlywheelVelocity(d: Double) = ((0.019454 * d * d) + (2.007 * d) + 1102.62509)
-    private fun calculateTurretAngle(vec: Vector) = Math.toDegrees(atan2(vec.yComponent, vec.xComponent) - follower.pose.heading)
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ROBOT.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ROBOT.kt
@@ -2,11 +2,16 @@ package org.firstinspires.ftc.teamcode.Util
 
 import com.pedropathing.geometry.Pose
 import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
+import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
 data object ROBOT {
     private const val TURRET_Y_OFFSET: Double = -1.774
+
+    var currAlliance: Alliance = Alliance.BLUE
+    var currStage: Stage = Stage.TELEOP
+    var teleopStartPose: Pose = Pose(0.0,0.0,0.0)
 
     fun shooterPose(): Pose {
         val a: Double = follower.heading
@@ -16,3 +21,34 @@ data object ROBOT {
         )
     }
 }
+
+enum class Alliance {
+    BLUE {
+        override val resetPoses: ResetPoses  = ResetPoses(Pose(9.5,8.9,-PI).mirror(), Pose(117.0,129.0,-2.448).mirror(), Pose(116.0,130.0,-0.939).mirror())
+        override val goalPoses: GoalPoses = GoalPoses(Pose(134.0,134.0).mirror(),Pose(140.0,140.0).mirror())
+    },
+    RED{
+        override val resetPoses: ResetPoses  = ResetPoses(Pose(9.5,8.9,-PI), Pose(117.0,129.0,-2.448), Pose(116.0,130.0,-0.939))
+        override val goalPoses: GoalPoses = GoalPoses(Pose(134.0,134.0),Pose(140.0,140.0))
+    };
+    abstract val resetPoses: ResetPoses
+    abstract val goalPoses: GoalPoses
+}
+
+enum class Motif {
+    UNKNOWN { override val tagID: Int = 0 },
+    PPG { override val tagID: Int = 23 },
+    PGP { override val tagID: Int = 22 },
+    GPP { override val tagID: Int = 21 };
+    abstract val tagID: Int
+}
+
+enum class Stage {
+    TELEOP,
+    AUTONOMOUS;
+    open var currMotif: Motif = Motif.UNKNOWN
+}
+
+data class GoalPoses(val turretGoalPose: Pose, val flywheelGoalPose: Pose)
+
+data class ResetPoses(val resetPose1: Pose, val resetPose2: Pose, val resetPose3: Pose)
