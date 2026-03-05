14a08752c7259191205f94242c4a1eeb137b2a72
AchintyaAkula <achintya.akula@gmail.com>
2026-03-04 17:50:04 -0800

    Changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Limelight.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Systems
+
+import com.qualcomm.hardware.limelightvision.Limelight3A
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.ftc.ActiveOpMode
+import org.firstinspires.ftc.teamcode.Util.Motif
+import org.firstinspires.ftc.teamcode.Util.ROBOT
+
+object Limelight: Subsystem {
+    val limelight: Limelight3A by lazy { ActiveOpMode.hardwareMap.get(Limelight3A::class.java, "limelight") }
+
+    const val LIMELIGHT_HEIGHT: Double = 0.0
+
+    fun detectMotif() {
+        val currTagID: Int = limelight.latestResult.fiducialResults[0].fiducialId
+        Motif.entries.forEach {
+            if (it.tagID == currTagID) {
+                ROBOT.currStage.currMotif = it
+            }
+        }
+    }
+    fun getBlobTurnAngle
+}
