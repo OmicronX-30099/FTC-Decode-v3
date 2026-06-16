3408aa6694b8b984358b9d3927952dc8f485bc36
AchintyaAkula <achintya.akula@gmail.com>
2026-06-15 21:51:09 -0700

    Save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Syntax.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Syntax.kt
@@ -1,5 +1,9 @@
 @file:Suppress("UNCHECKED_CAST", "SameParameterValue", "PackageDirectoryMismatch")
 
+import android.R.attr.data
+import android.R.attr.path
+import com.pedropathing.geometry.BezierCurve
+import com.pedropathing.geometry.BezierLine
 import com.pedropathing.geometry.Pose
 import com.pedropathing.paths.PathBuilder
 import com.pedropathing.paths.PathChain
@@ -39,23 +43,50 @@ object AutoManager {
         }
     }
 
-    private fun parsePathChain(data: Map<String, Any>): PathChain {
-        val pChainStart: PathBuilder = follower.pathBuilder()
+    private fun parsePathChain(data: Map<String, Any>)/*: PathChain*/ {
+        //val pChainStart: PathBuilder = follower.pathBuilder()
+        val paths = data["paths"] as ArrayList<Map<String, Any>>
+        for (path in paths) {
+            applyPath(path)
+        }
 
-
-
-        return pChainStart.build()
+        //return pChainStart.build()
     }
 
-    private fun applyPath(data: Map<String, Any>, currPChain: PathBuilder): PathBuilder {
+    private fun applyPath(data: Map<String, Any>, /*currPChain: PathBuilder*/)/*: PathBuilder*/ {
+        // Start point
+        val startPoint = parsePose(data["startPoint"] as Map<String, Any>)
+        val controlPoints: ArrayList<Pose> = arrayListOf(startPoint)
 
+        // Control Points
+        val controlPointData = data["controlPoints"] as ArrayList<Map<String, Any>>
+        for (point in controlPointData) { controlPoints.add(parsePose(point)) }
+
+        // End point
+        val endPoint = parsePose(data["endPoint"] as Map<String, Any>)
+        controlPoints.add(endPoint)
+        /*
+        // Apply to builder
+        when (controlPoints.size) {
+            2 -> {
+                currPChain.addPath(
+                    BezierLine(startPoint, endPoint)
+                )
+            }
+            else -> {
+                currPChain.addPath(
+                    BezierCurve(controlPoints)
+                )
+            }
+        }*/
+        println(controlPoints)
+        //return currPChain
     }
 
     private fun parsePose(data: Map<String, Any>)
         = Pose(
             roundTo((data["x"] as Number).toDouble(), 5),
             roundTo((data["y"] as Number).toDouble(), 5),
-            Math.toRadians(roundTo((data["heading"] as Number).toDouble(), 5))
         )
     private fun roundTo(num: Double, digits: Int): Double
         = round(num * 10.0.pow(digits)) / 10.0.pow(digits)
